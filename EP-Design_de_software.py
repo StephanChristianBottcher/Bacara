2fe534c6757c1b1d8109f010818a0ff569b2bbea
Stephan Christian Bottcher <stephan@al.insper.edu.br>
2020-10-18 16:40:22 -0300

    Embaralhamento das cartas

--- EP-Design_de_software.py
+++ EP-Design_de_software.py
@@ -2,9 +2,29 @@
 # Aluno: Stephan Christian Bottcher
 # Data: 17/10/2020
 
+#importando a funcao random para a distribuicao das cartas
+
 import random
 
+# inserindo baralho completo de 52 cartas
 cartas = [1,2,3,4,5,6,7,8,9,10,0,0,0]
+cartas = cartas*4
+
+# as duas cartas distribuidas ao jogador(a e b)
+random(cartas) = a
+random(cartas) = b
+
+# as duas cartas distribuidas ao banco(c e d)
+random(cartas) = c
+random(cartas) = d
+
+# as cartas adicionais possivelmente distribuidas ao jogador e banco, respectivamente(e e f)
+random(cartas) = e
+random(cartas) = f
+
+
+
+ficha = 300
 
 
 print ('Jogo de Bacara')
@@ -12,12 +32,13 @@ print ('Jogo de Bacara')
 objeto_da_aposta = input('Em quem deseja apostar (banco, jogador ou empate)? ')
 valor_da_aposta = input('Voce tem 300 fichas, quantas deseja apostar? ')
 
-if valor_da_aposta <= 300
+if valor_da_aposta <= ficha
     print('Aposta feita')
 else:
-    while valor_da_aposta > 300
+    while valor_da_aposta > ficha
      print('O valor da aposta excede a sua quantidade de fichas.')
-     valor_da_aposta = input('Voce tem 300 fichas, quantas deseja apostar? ')
+     valor_da_aposta = input('Voce tem {0} fichas, quantas deseja apostar? ')
+
 
 
     
