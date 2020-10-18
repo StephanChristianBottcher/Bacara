feb13d3894aac26cc3bcb579c5d42471ea1659db
Stephan Christian Bottcher <stephan@al.insper.edu.br>
2020-10-18 15:30:54 -0300

    Inicio do projeto

---
+++ EP-Design_de_software.py
@@ -0,0 +1,28 @@
+# EP - Design de software
+# Aluno: Stephan Christian Bottcher
+# Data: 17/10/2020
+
+import random
+
+cartas = [1,2,3,4,5,6,7,8,9,10,0,0,0]
+
+
+print ('Jogo de Bacara')
+
+objeto_da_aposta = input('Em quem deseja apostar (banco, jogador ou empate)? ')
+valor_da_aposta = input('Voce tem 300 fichas, quantas deseja apostar? ')
+
+if valor_da_aposta <= 300
+    print('Aposta feita')
+else:
+    while valor_da_aposta > 300
+     print('O valor da aposta excede a sua quantidade de fichas.')
+     valor_da_aposta = input('Voce tem 300 fichas, quantas deseja apostar? ')
+
+
+    
+
+    
+
+
+    
